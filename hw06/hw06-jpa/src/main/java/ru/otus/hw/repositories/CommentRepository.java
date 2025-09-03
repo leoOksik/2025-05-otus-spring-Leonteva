acadf41e176b8bfdda3024f3d91c9047afe38d98
Oksana <oksleontev@yandex.ru>
2025-09-03 14:40:01 +0300

    hw06 (version 1 )

---
+++ hw06/hw06-jpa/src/main/java/ru/otus/hw/repositories/CommentRepository.java
@@ -0,0 +1,16 @@
+package ru.otus.hw.repositories;
+
+import ru.otus.hw.models.Comment;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CommentRepository {
+    Optional<Comment> findById(Long id);
+
+    List<Comment> findByBookId(Long id);
+
+    Comment save(Comment comment);
+
+    void deleteById(Long id);
+}
---
+++ hw06/hw06-jpa/src/test/java/ru/otus/hw/services/CommentServiceImplTest.java
@@ -0,0 +1,55 @@
+package ru.otus.hw.services;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import ru.otus.hw.repositories.JpaBookRepository;
+import ru.otus.hw.repositories.JpaCommentRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+@DataJpaTest
+@Import({CommentServiceImpl.class, JpaCommentRepository.class, JpaBookRepository.class})
+class CommentServiceImplTest {
+
+    private static final Long GET_COMMENT_ID = 1L;
+    private static final Long GET_COMMENT_BOOK_ID = 1L;
+
+    @Autowired
+    private CommentServiceImpl commentService;
+
+    @Test
+    @DisplayName("должен загружать комментарий по id без LazyInitializationException")
+    void shouldReturnCommentByIdWithoutLazyException() {
+        var comment = commentService.findById(GET_COMMENT_ID).orElseThrow();
+
+        assertThat(comment).isNotNull();
+        assertDoesNotThrow(() -> {
+            var book = comment.getBook();
+            assertThat(book).isNotNull();
+            assertThat(book.getAuthor()).isNotNull();
+            assertThat(book.getGenres()).isNotEmpty();
+            assertThat(book.getComments()).isNotEmpty();
+        });
+    }
+
+    @Test
+    @DisplayName("должен загружать комментарии по id книги без LazyInitializationException")
+    void shouldReturnCommentsByBookIdWithoutLazyException() {
+        var comments = commentService.findByBookId(GET_COMMENT_BOOK_ID);
+
+        assertDoesNotThrow(() -> {
+            assertThat(comments).isNotNull().isNotEmpty();
+            comments.forEach(comment -> {
+                var book = comment.getBook();
+                assertThat(book).isNotNull();
+                assertThat(book.getAuthor()).isNotNull();
+                assertThat(book.getGenres()).isNotEmpty();
+                assertThat(book.getComments()).isNotEmpty();
+            });
+        });
+    }
+}
